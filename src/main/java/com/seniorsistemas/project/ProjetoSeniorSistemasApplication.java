c4829e4fd48be86169de23e2b1edf49b5ce87895
Matheus Streisky <streiskymatheus@gmail.com>
2024-09-19 01:14:11 -0300

    Adição do projeto

---
+++ src/main/java/com/seniorsistemas/project/ProjetoSeniorSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.seniorsistemas.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSeniorSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSeniorSistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seniorsistemas/project/ProjetoSeniorSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniorsistemas.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSeniorSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
